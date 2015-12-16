a5e5151022ca65740e4b504dc3133286315ff991
Christoph von Krüchten <cvkkoeln@gmail.com>
2015-12-16 04:37:28 -0800

    Solution for day 12, part 1

---
+++ src/Day12/Json.hs
@@ -0,0 +1,18 @@
+module Day12.Json where
+
+import Data.List (groupBy)
+import Data.Char (isDigit)
+import Data.Maybe (catMaybes)
+
+findNumbers :: String -> [Int]
+findNumbers xs = catMaybes $ map maybeRead $ groupBy f xs
+    where 
+        f a b = all (`elem` "0123456789-") [a,b]
+        maybeRead s = case reads s of
+            [(x, "")] -> Just x
+            _         -> Nothing
+
+main :: IO ()
+main = do
+    s <- readFile "src/Day12/data.txt"
+    print $ sum $ findNumbers s
