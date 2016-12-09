0f7bba4ff38b565c0f876b40de040dc4dd41b914
Christoph von Krüchten <christoph@vonkruechten.de>
2016-12-09 15:02:09 +0100

    Changed type to Char, because part 2

--- 2016/src/Day02/BathroomSecurity.hs
+++ 2016/src/Day02/BathroomSecurity.hs
@@ -4,7 +4,7 @@ import Prelude hiding (Left, Right)
 import Data.Maybe (mapMaybe)
 import qualified Data.Map as Map
 
-type Keypad = Map.Map Int (Int, Int, Int, Int)
+type Keypad = Map.Map Char (Char, Char, Char, Char)
 data Command = Up | Right | Down | Left 
     deriving (Show)
 
@@ -24,18 +24,18 @@ parseLine = mapMaybe parse
 keypad1 :: Keypad
 keypad1 = 
     Map.fromList 
-        [(1, (1, 2, 4, 1))
-        ,(2, (2, 3, 5, 1))
-        ,(3, (3, 3, 6, 2))
-        ,(4, (1, 5, 7, 4))
-        ,(5, (2, 6, 8, 4))
-        ,(6, (3, 6, 9, 5))
-        ,(7, (4, 8, 4, 4))
-        ,(8, (5, 9, 8, 7))
-        ,(9, (6, 9, 9, 8))
+        [('1', ('1', '2', '4', '1'))
+        ,('2', ('2', '3', '5', '1'))
+        ,('3', ('3', '3', '6', '2'))
+        ,('4', ('1', '5', '7', '4'))
+        ,('5', ('2', '6', '8', '4'))
+        ,('6', ('3', '6', '9', '5'))
+        ,('7', ('4', '8', '4', '4'))
+        ,('8', ('5', '9', '8', '7'))
+        ,('9', ('6', '9', '9', '8'))
         ]
 
-step :: Keypad -> Int -> Command -> Int
+step :: Keypad -> Char -> Command -> Char
 step kp n = nextKey
     where 
         (u, r, d, l) = Map.findWithDefault (n, n, n, n) n kp
@@ -44,14 +44,14 @@ step kp n = nextKey
         nextKey Left  = l
         nextKey Right = r
 
-follow :: Int -> [Command] -> Int
-follow = foldl (step keypad1) 
+follow :: Keypad -> Char -> [Command] -> Char
+follow kp = foldl (step kp) 
 
 puzzle1 cs pos = 
-    tail $ scanl follow pos (map parseLine cs)
+    tail $ scanl (follow keypad1) pos (map parseLine cs)
 
 main :: IO()
 main = do
     s <- readFile "src/Day02/data.txt"
-    print $ puzzle1 (lines s) 5
+    print $ puzzle1 (lines s) '5'
     
